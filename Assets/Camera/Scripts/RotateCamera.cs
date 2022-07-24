f96639d4dad4bc64782be3efd9ab996403f70bad
GinoxXP <ginoxxp@yandex.ru>
2022-07-24 10:50:34 +0300

    Add camera back to start position if player jump to branch

--- Assets/Camera/Scripts/RotateCamera.cs
+++ Assets/Camera/Scripts/RotateCamera.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,8 +11,35 @@ public class RotateCamera : MonoBehaviour
     private AnimationCurve speedMultiplicatorCurve;
 
     private Animator animator;
+    private IEnumerator animationSpeedCoroutine;
 
-    public void Death()
+    public void CameraBack()
+    {
+        animator.SetTrigger("CameraBack");
+        StopCoroutine(animationSpeedCoroutine);
+    }
+
+    public void StartSpeedMultiplicatorByTime()
+    {
+        animationSpeedCoroutine = AnimationSpeedMultiplicatorByTime();
+        StartCoroutine(animationSpeedCoroutine);
+    }
+
+    private IEnumerator AnimationSpeedMultiplicatorByTime()
+    {
+        var duration = 0f;
+        var maxDuration = speedMultiplicatorCurve.keys.LastOrDefault().time;
+        while (duration < maxDuration)
+        {
+            animator.speed = speedMultiplicatorCurve.Evaluate(duration);
+
+            duration += Time.deltaTime;
+        }
+
+        yield return null;
+    }
+
+    private void Death()
     {
         /// TODO 
         /// Remake this
@@ -20,13 +48,9 @@ public class RotateCamera : MonoBehaviour
         SceneManager.LoadScene(currentScene.name);
     }
 
-    public void CameraBack()
-    {
-        animator.SetTrigger("CameraBack");
-    }
-
     private void Start()
     {
         animator = GetComponent<Animator>();
+        StartSpeedMultiplicatorByTime();
     }
 }
