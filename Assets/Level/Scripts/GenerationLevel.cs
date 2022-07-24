281a01261d6953c422c7b604aff9a4874096f939
GinoxXP <ginoxxp@yandex.ru>
2022-07-24 15:11:12 +0300

    Add random offset for branches

--- Assets/Level/Scripts/GenerationLevel.cs
+++ Assets/Level/Scripts/GenerationLevel.cs
@@ -6,18 +6,22 @@ public class GenerationLevel : MonoBehaviour
 {
     private DiContainer container;
     private MoveLevel moveLevel;
+    private DifficultyManager difficultyManager;
 
     private System.Random random;
 
     [SerializeField]
     private List<GameObject> branches = new List<GameObject>();
+    [SerializeField]
+    private AnimationCurve maxRandomOffsetCurve;
 
     public void Generate()
     {
         var randomIndex = random.Next(branches.Count);
         var branch = container.InstantiatePrefab(branches[randomIndex], transform);
 
-        var newPosition = new Vector3(-5, branch.transform.position.y, branch.transform.position.z);
+        var randomOffset = (float) (random.NextDouble() - 0.5f) * maxRandomOffsetCurve.Evaluate(difficultyManager.Difficulty);
+        var newPosition = new Vector3(-5 + randomOffset, branch.transform.position.y, branch.transform.position.z);
         branch.transform.position = newPosition;
 
         moveLevel.Branches.Add(branch.transform);
@@ -29,9 +33,10 @@ public class GenerationLevel : MonoBehaviour
     }
 
     [Inject]
-    private void Init(MoveLevel moveLevel, DiContainer container)
+    private void Init(MoveLevel moveLevel, DiContainer container, DifficultyManager difficultyManager)
     {
         this.moveLevel = moveLevel;
         this.container = container;
+        this.difficultyManager = difficultyManager;
     }
 }
