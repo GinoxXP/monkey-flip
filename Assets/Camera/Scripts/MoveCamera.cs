8d22a4ff27e3e53439283e04fbe154bdc7f7911c
GinoxXP <ginoxxp@yandex.ru>
2022-08-07 15:22:58 +0300

    Add death by moving camera

--- Assets/Camera/Scripts/MoveCamera.cs
+++ Assets/Camera/Scripts/MoveCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MoveCamera : MonoBehaviour
 {
@@ -19,42 +20,6 @@ public class MoveCamera : MonoBehaviour
 
     private IEnumerator moveCoroutine;
     private IEnumerator returnCoroutine;
-
-    private IEnumerator MoveAnimationByTime()
-    {
-        var distanceBetweenPoints = Vector3.Distance(startPosition, endPosition);
-
-        while(true)
-        {
-            var distanceToEnd = Vector3.Distance(transform.position, endPosition);
-            if (distanceToEnd == MIN_TRESHOLD)
-                break;
-
-            var completenessCoef = 1 - Mathf.Clamp(distanceToEnd, 0, distanceBetweenPoints) / distanceBetweenPoints;
-
-            var newPosition = Vector3.MoveTowards(transform.position, endPosition, speed * speedCurve.Evaluate(completenessCoef));
-            transform.position = newPosition;
-
-            yield return null;
-        }
-
-        yield return null;
-    }
-
-    private IEnumerator ReturnAnimationByTime()
-    {
-        while (Vector3.Distance(transform.position, startPosition) > MIN_TRESHOLD)
-        {
-            var newPosition = Vector3.MoveTowards(transform.position, startPosition, returnSpeed);
-            transform.position = newPosition;
-
-            yield return null;
-        }
-
-        StartMove();
-
-        yield return null;
-    }
 
     public void StopMove()
     {
@@ -75,5 +40,47 @@ public class MoveCamera : MonoBehaviour
     {
         returnCoroutine = ReturnAnimationByTime();
         StartCoroutine(returnCoroutine);
+    }
+
+    private IEnumerator MoveAnimationByTime()
+    {
+        var distanceBetweenPoints = Vector3.Distance(startPosition, endPosition);
+
+        while(true)
+        {
+            var distanceToEnd = Vector3.Distance(transform.position, endPosition);
+            if (distanceToEnd < MIN_TRESHOLD)
+                break;
+
+            var completenessCoef = 1 - Mathf.Clamp(distanceToEnd, 0, distanceBetweenPoints) / distanceBetweenPoints;
+
+            var newPosition = Vector3.MoveTowards(transform.position, endPosition, speed * speedCurve.Evaluate(completenessCoef));
+            transform.position = newPosition;
+
+            yield return null;
+        }
+
+        Death();
+    }
+
+    private IEnumerator ReturnAnimationByTime()
+    {
+        while (Vector3.Distance(transform.position, startPosition) > MIN_TRESHOLD)
+        {
+            var newPosition = Vector3.MoveTowards(transform.position, startPosition, returnSpeed);
+            transform.position = newPosition;
+
+            yield return null;
+        }
+
+        StartMove();
+
+        yield return null;
+    }
+
+    private void Death()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 }
