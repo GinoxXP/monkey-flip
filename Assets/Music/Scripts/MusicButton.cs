df779e33737163d570df6144fc911534c66821e2
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-01-28 17:32:52 +0300

    Redisign music and sound button

--- Assets/Music/Scripts/MusicButton.cs
+++ Assets/Music/Scripts/MusicButton.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
-using Zenject;
 
 [RequireComponent(typeof(Image))]
 public class MusicButton : MonoBehaviour
@@ -10,15 +9,10 @@ public class MusicButton : MonoBehaviour
     private const float DISABLE_MUSIC_VOLUME = -80;
     private const float ENABLE_MUSIC_VOLUME = 0;
 
-    private PauseController pauseController;
-    private Image image;
-
     private bool isOn = true;
 
     [SerializeField]
-    private Sprite onStateSprite;
-    [SerializeField]
-    private Sprite offStateSprite;
+    private GameObject line;
     [SerializeField]
     private AudioMixer musicMixer;
     [SerializeField]
@@ -30,39 +24,21 @@ public class MusicButton : MonoBehaviour
         set
         {
             isOn = value;
+            line.SetActive(!isOn);
             PlayerPrefs.SetInt(KEY, isOn ? 1 : 0);
+
             if (isOn)
-            {
-                image.sprite = onStateSprite;
                 musicMixer.SetFloat(VOLUME_PARAMETER, ENABLE_MUSIC_VOLUME);
-            }
             else
-            {
-                image.sprite = offStateSprite;
                 musicMixer.SetFloat(VOLUME_PARAMETER, DISABLE_MUSIC_VOLUME);
-            }
         }
     }
 
     public void Click()
         => IsOn = !IsOn;
 
-    private void OnPauseChanged(bool isPause)
-    {
-        gameObject.SetActive(isPause);
-    }
-
     private void Start()
     {
-        image = GetComponent<Image>();
-
         IsOn = PlayerPrefs.GetInt(KEY, 1) == 1;
-    }
-
-    [Inject]
-    private void Init(PauseController pauseController)
-    {
-        this.pauseController = pauseController;
-        pauseController.PauseChanged += OnPauseChanged;
     }
 }
